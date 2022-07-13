a6dc376a727ad5db51172d0f56c061301d4ca4a9
Spencer Harris <morrisonleary@gmail.com>
2022-07-13 12:15:19 -0400

    removed comment

--- server/src/index.js
+++ server/src/index.js
@@ -8,9 +8,6 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-// const MONGO_URL =
-//     'mongodb+srv://nasa-api:6LT52otvwPVAYEuI@nasacluster.m1ycwwz.mongodb.net/nasa?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
 
 // mongoose.connection.once('open', () => {
